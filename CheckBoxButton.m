cdbe0eeb33f8a14417795d4fd895eaeed4f075f0
AkiraYoshihara <immortal0751@yahoo.co.jp>
2012-12-03 04:29:26 +0900

    update

---
+++ CheckBoxButton.m
@@ -0,0 +1,31 @@
+//
+//  CheckBoxButton.m
+//  timemanagement
+//
+//  Created by AkiraYoshihara on 12/11/30.
+//  Copyright (c) 2012年 AkiraYoshihara. All rights reserved.
+//
+
+#import "CheckBoxButton.h"
+
+@implementation CheckBoxButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
